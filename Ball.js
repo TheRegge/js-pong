7fbf56b4263f267ed1c63c7f03489e08d5f5d94c
Regis Zaleman <regis.zaleman@gmail.com>
2021-12-08 22:02:10 -0500

    style: :art: Add Prettier config
    No semi, singleQuote

--- Ball.js
+++ Ball.js
@@ -8,19 +8,19 @@ export default class Ball {
   }
 
   get x() {
-    return parseFloat(getComputedStyle(this.ballElem).getPropertyValue("--x"))
+    return parseFloat(getComputedStyle(this.ballElem).getPropertyValue('--x'))
   }
 
   set x(value) {
-    this.ballElem.style.setProperty("--x", value)
+    this.ballElem.style.setProperty('--x', value)
   }
 
   get y() {
-    return parseFloat(getComputedStyle(this.ballElem).getPropertyValue("--y"))
+    return parseFloat(getComputedStyle(this.ballElem).getPropertyValue('--y'))
   }
 
   set y(value) {
-    this.ballElem.style.setProperty("--y", value)
+    this.ballElem.style.setProperty('--y', value)
   }
 
   rect() {
--- Paddle.js
+++ Paddle.js
@@ -8,12 +8,12 @@ export default class Paddle {
 
   get position() {
     return parseFloat(
-      getComputedStyle(this.paddleElem).getPropertyValue("--position")
+      getComputedStyle(this.paddleElem).getPropertyValue('--position')
     )
   }
 
   set position(value) {
-    this.paddleElem.style.setProperty("--position", value)
+    this.paddleElem.style.setProperty('--position', value)
   }
 
   rect() {
